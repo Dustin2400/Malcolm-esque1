53da7ce7501d0d3fb1f52b782b95e9d49e56968e
Dustin <dustin2400@hotmail.com>
2022-11-27 21:39:45 -0600

    c

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const URI = 'mongodb+srv://dustin2400:Pericles8@cluster0.gozfq.mongodb.net/malcolm-esque?retryWrites=true&w=majority'
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://dustin2400:Pericles8@cluster0.gozfq.mongodb.net/malcolm-esque?retryWrites=true&w=majority',
+  "mongodb+srv://david60:Pericles8@cluster0.gozfq.mongodb.net/?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
