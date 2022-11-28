f939bf41c6ce15a5feca00c9324340586eb5c1ff
Dustin <dustin2400@hotmail.com>
2022-11-27 20:25:16 -0600

    b

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/malcolm-esque',
+  process.env.MONGODB_URI || 'mongodb+srv://dustin2400:Pericles8@cluster0.gozfq.mongodb.net/budget-tracker?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
