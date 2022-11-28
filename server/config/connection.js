a6cfee7d50d42c94ef5afd19f08d7b7f4187b09e
Dustin <dustin2400@hotmail.com>
2022-11-27 22:27:20 -0600

    mistake on purpose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb+srv://dustin2400:Pericles8@cluster0.gozfq.mongodb.net/malcolm-esque?retryWrites=true&w=majority'
 
 mongoose.connect(
-  process.env.MONGODB_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-    // family: 4
-  }
+  // process.env.MONGODB_URI,
+  // {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   useFindAndModify: false
+  //   // family: 4
+  // }
 );
 
 module.exports = mongoose.connection;
