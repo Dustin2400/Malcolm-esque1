aeeeaf574984c6c7ddc1873b01ba5c124b160ccb
Dustin <dustin2400@hotmail.com>
2022-11-27 22:34:51 -0600

    mistake on purpose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  "mongodb+srv://dustin2400:Themistokles8@cluster0.gozfq.mongodb.net/budget-tracker?",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-    // family: 4
-  }
+  "mongodb+srv://dustin2400:Themistokles8@cluster0.gozfq.mongodb.net/budget-tracker?"
+  // {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   useFindAndModify: false
+  //   // family: 4
+  // }
 );
 
 module.exports = mongoose.connection;
